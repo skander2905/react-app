02a01207e053034c36f43669ab39a18118d2438d
skander2905 <skander.abid@ensi-uma.tn>
2022-07-19 03:56:29 +0100

    changes

--- src/component/CreatePost.js
+++ src/component/CreatePost.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const CreatePost = () =>{
-    return(
-     <div className="new-post-modal">
-        <form>
-            <textarea placeholder='Comment vas-tu ce jour ?'></textarea>
-            <input type="submit" value="Partager"></input>
-        </form>
-     </div>
+const CreatePost = () => {
+    return (
+        <div className="new-post-modal">
+            <form>
+                <textarea placeholder='Comment vas-tu ce jour ?'></textarea>
+                <input type="submit" value="Partager"></input>
+            </form>
+        </div>
     );
 };
 
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -2,41 +2,41 @@ import React, { useRef, useState } from 'react';
 import { auth } from "../utils/firebase.config";
 
 const SignUp = () => {
-  //  const registerEmail = useRef(); //recuperer les information tapees dans les inputs
-        //recuperer la valeur dans useref
+    //  const registerEmail = useRef(); //recuperer les information tapees dans les inputs
+    //recuperer la valeur dans useref
     const registerEmail = useRef();
     const registerPassword = useRef();
     const [displayName, setDisplayName] = useState(""); //on incremente displayname lorsque on ecrit le pseudo
     const handleRegister = (e) => {
         e.preventDefault();
-        try{ //essayer d'incrementer les choses suiv dans la bd
-         
+        try { //essayer d'incrementer les choses suiv dans la bd
+
             auth.createUserWithEmailAndPassword(
                 registerEmail.current.value,
                 registerPassword.current.value
-                )
+            )
                 .then(async (userAuth) => {
-                  await userAuth.user.updateProfile({
-                    displayName,
-                 });
-                    console.log(userAuth); 
+                    await userAuth.user.updateProfile({
+                        displayName,
+                    });
+                    console.log(userAuth);
                     window.location.reload();
                 });
-               
-        } catch(error){
+
+        } catch (error) {
             console.log(error.message);
         }
     };
 
-           return (
+    return (
         <div className='signup-container'>
             <div className='signup'>
                 <h3>S'inscrire</h3>
                 <form onSubmit={e => handleRegister(e)}>
-                   <input class ="text" type="text" placeholder='Pseudo' required onChange={(e) => setDisplayName(e.target.value)} />
-                   <input class ="text" type="email" placeholder="Email" required ref={registerEmail}  /> 
-                   <input class ="text" type="password" placeholder="Mot de passe" required ref={registerPassword}  />
-                   <input class ="text" type="submit" value="Valider inscription" />
+                    <input class="text" type="text" placeholder='Pseudo' required onChange={(e) => setDisplayName(e.target.value)} />
+                    <input class="text" type="email" placeholder="Email" required ref={registerEmail} />
+                    <input class="text" type="password" placeholder="Mot de passe" required ref={registerPassword} />
+                    <input class="text" type="submit" value="Valider inscription" />
                 </form>
             </div>
         </div>
