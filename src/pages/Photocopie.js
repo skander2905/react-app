f9c7d3c528a46ff2f76b0dcd0194e9e0392a3e32
skander2905 <skander.abid@ensi-uma.tn>
2022-08-02 11:49:36 +0100

    changes3

--- src/pages/Photocopie.js
+++ src/pages/Photocopie.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import SideMenu from '../component/SideMenu';
 import "./Type_dossier.css";
 
 const Photocopie = () => {
     return (
         <div>
-            
             <div className='page'>
                 <h1>Photocopie</h1>
             </div>
